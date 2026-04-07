ca060e3658176a7bc5429d5900047544316d1287
umhisa0301 <u.m.hisa.0301@gmail.com>
2026-04-07 23:47:05 +0900

    Add signing configuration to Android build.gradle.kts and update pubspec.yaml version
    This commit introduces a signing configuration for the release build in the Android `build.gradle.kts`, allowing for secure app signing using properties from a `key.properties` file. Additionally, the `pubspec.yaml` file is updated to increment the version number from 1.0.0+1 to 1.0.0+2, ensuring proper versioning for the Flutter project. These changes enhance the build process and maintain version control for future releases.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -41,4 +44,28 @@ android {
 
 flutter {
     source = "../.."
+}
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+android {
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+    }
 }
