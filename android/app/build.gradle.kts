21b170d250bf43d61670dc1b1dc10e2bfc94066f
umhisa0301 <u.m.hisa.0301@gmail.com>
2026-04-07 23:53:36 +0900

    Update Android namespace and application ID in build.gradle.kts for project rebranding

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.room_manager2"
+    namespace = "com.stepbytestudio.room_manager2"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.room_manager2"
+        applicationId = "com.stepbytestudio.room_manager2"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ android/app/src/main/kotlin/com/stepbytestudio/room_manager2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.stepbytestudio.room_manager2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
